3ed2a54f4fa5126f84dccfe4a10e7c9b88ca5c1c
iliashka <69167918+iliashka@users.noreply.github.com>
2020-10-20 19:34:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let s of str) {
+     if (s == '(' || s == '[' || s == '{') {
+        stack.push(s);
+        continue; 
+     }
+
+     if (stack.length === 0) {
+         return false
+     }
+
+     switch (s) {
+         case ')':
+              stack.pop();
+              if (s == '{' || s == '[') {
+                return false  
+              }
+              break;
+         case '}':
+              stack.pop();
+              if (s == '(' || s == '[') {
+                return false  
+              }
+              break;
+         case ']':
+              stack.pop();
+              if (s == '{' || s == '(') {
+                return false  
+              }
+              break;
+     }
+  }
+  return stack.length ? false : true
 }
